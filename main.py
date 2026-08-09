e84ef05bb36316bd32fd81537ea5d81f22d947c1
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-08-09 13:10:09 +0530

    materices.py(V2)  is complete

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from solver.basic_math import calculate
 from solver.tables import multiplication_table
 from solver.statistics import calculate_statistics
 from solver.algebra import solve_math
-# from solver.matrices import (
-#     create_matrix,
-#     add_matrices,
-#     subtract_matrices,
-#     multiply_matrices,
-#     transpose_matrix,
-#     determinant,
-#     inverse_matrix,
-#     matrix_trace
-# )
+from solver.matrices import (
+    create_matrix,
+    add_matrices,
+    subtract_matrices,
+    multiply_matrices,
+    transpose_matrix,
+    determinant,
+    inverse_matrix,
+    matrix_trace
+)
+
 print("========== AI MATH SOLVER ==========")
 
 while True:
@@ -57,7 +58,72 @@ while True:
 
     elif choice == "4":
 
-        print("Matrix Solver Coming Soon...")
+        print("\n========== MATRIX SOLVER ==========")
+
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Transpose")
+        print("5. Determinant")
+        print("6. Inverse")
+        print("7. Trace")
+        print("0. Back")
+
+        matrix_choice = input("\nEnter your choice: ")
+
+        A = create_matrix([
+            [1, 2],
+            [3, 4]
+        ])
+
+        B = create_matrix([
+            [5, 6],
+            [7, 8]
+        ])
+
+        if matrix_choice == "1":
+
+            print("\nMatrix A + Matrix B:")
+            print(add_matrices(A, B))
+
+        elif matrix_choice == "2":
+
+            print("\nMatrix A - Matrix B:")
+            print(subtract_matrices(A, B))
+
+        elif matrix_choice == "3":
+
+            print("\nMatrix A × Matrix B:")
+            print(multiply_matrices(A, B))
+
+        elif matrix_choice == "4":
+
+            print("\nTranspose of Matrix A:")
+            print(transpose_matrix(A))
+
+        elif matrix_choice == "5":
+
+            print("\nDeterminant of Matrix A:")
+            print(determinant(A))
+
+        elif matrix_choice == "6":
+
+            print("\nInverse of Matrix A:")
+            print(inverse_matrix(A))
+
+        elif matrix_choice == "7":
+
+            print("\nTrace of Matrix A:")
+            print(matrix_trace(A))
+
+        elif matrix_choice == "0":
+
+            print("Returning to main menu...")
+
+        else:
+
+            print("Invalid matrix choice.")
+
 
     elif choice == "5":
 
